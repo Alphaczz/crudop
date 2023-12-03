464ecf4905a03af6856aa733607a4c30fb9e5a52
shiva <shivam212002@gmail.com>
2023-12-03 20:32:25 +0530

    2nd commit

---
+++ src/main/java/com/example/crudop/domain/repository/util/utilResource.kt
@@ -0,0 +1,16 @@
+package com.example.crudop.domain.repository.util
+
+import com.example.crudop.data.model.PostModel
+import java.util.UUID
+
+
+    fun generateUniqueId(): Long {
+        val uuid = UUID.randomUUID()
+        return uuid.leastSignificantBits
+    }
+    fun createPost( postTitle:String,
+                    posterDes:String,
+                    author:String,
+    ): PostModel {
+        return PostModel(postid = generateUniqueId() , postTitle=postTitle, posterDes = posterDes, author = author, likes = 0, dislike = 0)
+    }
--- src/main/java/com/example/crudop/presentation/ui/screen/Activities/createPost.kt
+++ src/main/java/com/example/crudop/presentation/ui/screen/Activities/createPost.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.crudop.MainActivity
 import com.example.crudop.data.model.PostModel
 import com.example.crudop.databinding.ActivityCreatePostBinding
+import com.example.crudop.domain.repository.util.createPost
 import com.example.crudop.presentation.viewmodel.CreatePostViewModel
 import com.example.crudop.presentation.viewmodel.PostViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,33 +29,64 @@ class CreatePost : AppCompatActivity() {
         binding = ActivityCreatePostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-            val titleEditText: EditText = binding.editTextPostTitle
-            val descriptionEditText: EditText = binding.editTextPostDescription
-            val authorEditText: EditText = binding.editTextPostAuthor
+        val titleEditText: EditText = binding.editTextPostTitle
+        val descriptionEditText: EditText = binding.editTextPostDescription
+        val authorEditText: EditText = binding.editTextPostAuthor
 
 
-            val saveButton: Button = binding.buttonSubmit
-binding.homeIcon.setOnClickListener {
-    navigateToMainActivity()
-}
-        saveButton.setOnClickListener {
-        val title = titleEditText.text.toString()
-        val description = descriptionEditText.text.toString()
-        val author = authorEditText.text.toString()
-
-        val post = createPost(title, description, author)
-
-        CoroutineScope(Dispatchers.Default).launch {
-            viewModel.savePost(post)
+        val saveButton: Button = binding.buttonSubmit
+        binding.homeIcon.setOnClickListener {
             navigateToMainActivity()
         }
+        saveButton.setOnClickListener {
+            val title = titleEditText.text.toString()
+            val description = descriptionEditText.text.toString()
+            val author = authorEditText.text.toString()
 
+            val post = createPost(title, description, author)
+            fun isValidInput(title: String, description: String, author: String): Boolean {
+                return when {
+                    title.isEmpty() -> {
+                        titleEditText.error = "Title cannot be empty"
+                        false
+                    }
+                    description.isEmpty() -> {
+                        descriptionEditText.error = "Description cannot be empty"
+                        false
+                    }
+                    author.isEmpty() -> {
+                        authorEditText.error = "Author cannot be empty"
+                        false
+                    }
+                    else -> true
+                }
+            }
+            if (isValidInput(title, description, author)) {
+                val post = createPost(title, description, author)
 
+                CoroutineScope(Dispatchers.Default).launch {
+                    viewModel.savePost(post)
+                    navigateToMainActivity()
+                }
+            } else {
+                var errorMessage = "Please fix the following issues:\n"
 
+                if (title.isEmpty()) {
+                    errorMessage += "- Title cannot be empty\n"
+                }
 
-}
+                if (description.isEmpty()) {
+                    errorMessage += "- Description cannot be empty\n"
+                }
 
+                if (author.isEmpty()) {
+                    errorMessage += "- Author cannot be empty\n"
+                }
 
+                // Display a Toast with the error message
+                Toast.makeText(this@CreatePost, errorMessage, Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
     fun navigateToMainActivity() {
@@ -64,13 +97,5 @@ binding.homeIcon.setOnClickListener {
 
 
 }
-fun createPost( postTitle:String,
-                posterDes:String,
-                author:String,
-): PostModel {
-    return PostModel(postid = generateUniqueId() , postTitle=postTitle, posterDes = posterDes, author = author, likes = 0, dislike = 0)
-}
-fun generateUniqueId(): Long {
-    val uuid = UUID.randomUUID()
-    return uuid.leastSignificantBits
-}
+
+
